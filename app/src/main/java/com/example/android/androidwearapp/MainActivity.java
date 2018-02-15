44b2ce91ed6e945ccd005f4efdc679b98c118289
ahmed fadhil <fadhil@fbk.eu>
2018-02-15 23:15:46 +0100

    added card into wear

--- app/src/main/java/com/example/android/androidwearapp/MainActivity.java
+++ app/src/main/java/com/example/android/androidwearapp/MainActivity.java
@@ -1,32 +1,44 @@
 package com.example.android.androidwearapp;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
-import android.view.View;
+import android.support.wearable.view.CardFragment;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
     private TextView mTextView;
-    int counter = 0;
-
-    public void plusOne(View view) {
-        counter++;
-        mTextView.setText(Integer.toString(counter));
-    }
-
-    public void reset(View view) {
-        counter = 0;
-        mTextView.setText(Integer.toString(counter));
-    }
+//    int counter = 0;
+//
+//    public void plusOne(View view) {
+//        counter++;
+//        mTextView.setText(Integer.toString(counter));
+//    }
+//
+//    public void reset(View view) {
+//        counter = 0;
+//        mTextView.setText(Integer.toString(counter));
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.mTextView);
+//        mTextView = (TextView) findViewById(R.id.mTextView);
 
         // Enables Always-on
         setAmbientEnabled();
-    }
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        CardFragment cardFragment = CardFragment.create("Hello Rob", "How are you doing?", android.R.drawable.btn_dialog);
+
+
+        fragmentTransaction.add(R.id.frame_layout, cardFragment);
+        fragmentTransaction.commit();
+
+    };
+
+
 }
